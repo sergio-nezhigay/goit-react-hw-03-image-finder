91bce722b84649fdbaca7e6af97497680c8843bf
Sergio Nezhigay <abc1971h@gmail.com>
2023-04-19 18:06:22 +0300

    init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Container, Section } from 'components';
+
+export class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Section title="Phonebook">
+          <h1>Test!!!!!!!!!!</h1>
+        </Section>
+      </Container>
+    );
+  }
+}
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Error = styled.p`
+  background-color: #f8d7da;
+  color: #721c24;
+  border: 1px solid #f5c6cb;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
